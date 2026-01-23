3d82ee0e48311c12fa3b3630f556c225e4ddad01
mehatab <mehatab661@gmail.com>
2026-01-23 16:48:09 +0530

    created react and js updated

--- script.js
+++ script.js
@@ -141,54 +141,187 @@
 // let gra = (marks>=90)?"o grade":"A grade";
 // console.log(res);
 // console.log(gra);
-function add(){
-    console.log(10+40);
+// function add(){
+//     console.log(10+40);
     
-}
-add();  
-//ARROW FUNCTION
-var add = (a,b)=>{
-    console.log(a+b);
+// }
+// add();  
+// //ARROW FUNCTION
+// var add = (a,b)=>{
+//     console.log(a+b);
     
-}
-add();
-add(10,20);
-var c;
-function add(a,b){;
-    c =10
-    console.log(a+b+c);
+// }
+// add();
+// add(10,20);
+// var c;
+// function add(a,b){;
+//     c =10
+//     console.log(a+b+c);
     
-}
-add(10,20)
-//hosting
-var z;
-console.log(z);
-//spread operator
-var arr1 = [10,20,30];
-var arr2 = [...arr1,40,50,60];//3 dots are called spread operator
-console.log(arr2);
-//destructuring operator
-var [m1,m2,m3,m4,m5] = [89,90,99,96,87];
-console.log(m1);
-console.log(m2);
-console.log(m3);
-console.log(m4);
-console.log(m5); 
-  var {name,age,dept}= {
-    name:'meha',
-    age:20,
-    dept:['csd','ece'],
-    marks : {
-        mern:90,
-        java:89,
-        c:80
-    }
-  }
-  console.log(name);
-  console.log(age);
-  console.log(dept);
-  console.log(marks);
-  
+// }
+// add(10,20)
+// //hosting
+// var z;
+// console.log(z);
+// //spread operator
+// var arr1 = [10,20,30];
+// var arr2 = [...arr1,40,50,60];//3 dots are called spread operator
+// console.log(arr2);
+// //destructuring operator
+// var [m1,m2,m3,m4,m5] = [89,90,99,96,87];
+// console.log(m1);
+// console.log(m2);
+// console.log(m3);
+// console.log(m4);
+// console.log(m5); 
+//   var {name,age,dept}= {
+//     name:'meha',
+//     age:20,
+//     dept:['csd','ece'],
+//     marks : {
+//         mern:90,
+//         java:89,
+//         c:80
+//     }
+//   }
+//   console.log(name);
+//   console.log(age);
+//   console.log(dept);
+//   console.log(marks);
+//FOR IN LOOP
+// let arr = [10,20,30,40];
+// for (let i in arr){
+//     console.log(i,arr[i]);
+// }
+// var obj = {
+//     name : 'meha',
+//     age : 20
+// }
+// for(let i in obj){
+//     console.log(i,obj[i]);
+    
+
+// }
+// // For of
+// //for(variable variable_name of array_name)
+// for (let val of arr){
+//     console.log(val);
+    
+
+//MAP
+// let arr = [10,20,30,40];
+
+// var doubled_arr = arr.map((i)=>((i*2)));
+// console.log(doubled_arr);
+// let arr = [1,2,3,4]
+// var even = arr.filter((i)=>(i%2==0))
+// console.log(even);
+// var total = arr.reduce((sum ,i)=>(sum+i),0);
+// // console.log(total);
+// let arr = [1,2,3,4]
+// var doubled_arr = arr.map((i)=>(i*2));
+// console.log(doubled_arr);
+// var even = doubled_arr.filter((i)=>(i%2==0))
+// console.log(even);
+// var tot = doubled_arr.reduce((sum,i)=>(sum+i),0);
+// console.log(tot);
+// tot_sum = arr.map((num)=>num*2).filter((num)=>(num%2==0)).reduce(sum,num)=>(sum+num)
+// var n = 7;
+// var flag = true;
+// if(n<=1) {
+//     flag = false;
+// }
+// else{
+//     for(let i = 2;i<n/2;i++){
+//         if(n%i==0){
+//             flag = false;
+//             break;
+//         }
+//     }
+// }
+// console.log(flag?"prime":"not prime");
+// var add = (a,b,callback)=>{
+//     var result = a+b;
+//     callback(result);
+// }
+// add(10,20,(res)=>{console.log(res)})
+// var main=(callback)=>{
+//     console.log("I am main");
+//     callback()
+    
+// }
+// var demo =()=>{
+// //     console.log("I am Call back");
+    
+// // }
+// // main(demo)
+// var  demo = ()=>{
+//     console.log("I am callback");
+    
+// }
+// var main = (myFunction)=>{
+//     myFunction()
+// }
+// main(demo);
+// PROMISE
+
+
+// var promise = new Promise((resolve,reject)=>{
+//     var success = true;
+//     if(success){
+//         setTimeout(()=>{
+//             resolve("promise resolved");
+
+//         },5000)
+        
+//     }
+//     else{
+//         reject("promise rejected");
+//     }
+// })
+// promise.then((res)=>console.log(res))
+// .catch((err)=>console.log(err))
+
+// // PROMISE
+
+
+// var promise = new Promise((resolve,reject)=>{
+//     var success = true;
+//     if(success){
+//         resolve("promise resolved");
+//     }
+//     else{
+//         reject("promise rejected");
+//     }
+// })
+// promise.then((res)=>console.log(res))
+// .catch((err)=>console.log(err))
+// getting json files
+
+// const getData =()=>{
+//     return fetch( 'https://jsonplaceholder.typicode.com/posts')
+// }
+// getData().then((res)=>res.json())
+// .then((data)=>console.log(data))
+// .catch(err=>console.log(err))
+
+// const getData = async ()=>{
+//     try{
+//     var res = await fetch('https://jsonplaceholder.typicode.com/posts')
+//     var data = await res.json();
+//     console.log(data);
+    
+//     }catch(err){
+//         console.log(err);
+        
+//     }
+// }
+// getData();
+
+
+
+
+
   
   
   
