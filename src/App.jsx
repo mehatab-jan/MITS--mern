2774bfb823f0320e66b01ac552189617298ab9af
mehatab <mehatab661@gmail.com>
2026-02-06 16:24:35 +0530

    connected with todo

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+// 
+
+
+
+
+
+
+
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './component/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Service from './pages/Service'
+import UseState from './Hooks/UseState'
+import Login from './Auth/Login'
+import Hook from './Hooks/Hook'
+import UseEffect from './Hooks/UseEffect'
+import UseEffectApi from './Hooks/UseEffectApi'
+import Signup from './Auth/Signup'
+import UseRef from './Hooks/UseRef'
+import UseReducer from './Hooks/UseReducer'
+import UseMemo from './Hooks/UseMemo'
+import UseCallback from './Hooks/UseCallback'
+import Todo from './component/Todo'   // ✅ FIXED (React component)
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home name="Jaaan" />} />
+        <Route path="/about" element={<About name="Jaaan" age={20} dept="CSD" />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/todo" element={<Todo />} />
+
+        {/* Hooks */}
+        <Route path="/hook" element={<Hook />}>
+          <Route path="useState" element={<UseState />} />
+          <Route path="useEffect" element={<UseEffect />} />
+          <Route path="useEffectApi" element={<UseEffectApi />} />
+          <Route path="useRef" element={<UseRef />} />
+          <Route path="useReducer" element={<UseReducer />} />
+          <Route path="useMemo" element={<UseMemo />} />
+          <Route path="useCallback" element={<UseCallback />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
+
+import App from './App.jsx'
+import ThemeProvider from './context/ThemeProvider.jsx'
+import AuthProvider from './context/AuthProvider.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <ThemeProvider>
+      <AuthProvider>
+       <App />
+    </AuthProvider>
+     
+    </ThemeProvider>
+  
+    </BrowserRouter>
+  
+  </StrictMode>,
+)
