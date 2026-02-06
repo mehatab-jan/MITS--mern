4e5e0a4c5f484d2c9db17c6bb87fda54a90b9b01
mehatab <mehatab661@gmail.com>
2026-02-06 15:59:05 +0530

    connected with todo

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -9,7 +9,6 @@ import AuthProvider from './context/AuthProvider.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    
     <ThemeProvider>
       <AuthProvider>
        <App />
