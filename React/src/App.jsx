f61931908972ae75bcbb949b91b7d20bd5744761
mehatab <mehatab661@gmail.com>
2026-02-06 16:48:20 +0530

    connected with todo

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './component/Navbar'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-import Service from './Pages/Service'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Service from './pages/Service'
 import UseState from './Hooks/UseState'
 import Login from './Auth/Login'
 import Hook from './Hooks/Hook'
@@ -14,35 +14,31 @@ import UseRef from './Hooks/UseRef'
 import UseReducer from './Hooks/UseReducer'
 import UseMemo from './Hooks/UseMemo'
 import UseCallback from './Hooks/UseCallback'
-import Todo from './component/Todo'   // ✅ FIXED (React component)
-
-const App = () => {
-  return (
+import Todo from './component/Todo'
+const App = ()=>{
+  return(
     <>
-      <Navbar />
-
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<Home name="Jaaan" />} />
-        <Route path="/about" element={<About name="Jaaan" age={20} dept="CSD" />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Service />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/todo" element={<Todo />} />
-
+        <Route path='/' element={<Home name="vikraam"/>}/>
+        <Route path='/about' element={<About name="Vikraam" age={25} dept="CS&DS"/>}></Route>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/service' element={<Service/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/todo' element={<Todo/>}/>
         {/* Hooks */}
-        <Route path="/hook" element={<Hook />}>
-          <Route path="useState" element={<UseState />} />
-          <Route path="useEffect" element={<UseEffect />} />
-          <Route path="useEffectApi" element={<UseEffectApi />} />
-          <Route path="useRef" element={<UseRef />} />
-          <Route path="useReducer" element={<UseReducer />} />
-          <Route path="useMemo" element={<UseMemo />} />
-          <Route path="useCallback" element={<UseCallback />} />
+        <Route path='/hook' element={<Hook/>}>
+          <Route path='useState' element={<UseState/>}/>
+          <Route path='useEffect' element={<UseEffect/>}/>
+          <Route path='useEffectApi' element={<UseEffectApi/>}/>
+          <Route path='useRef' element={<UseRef/>}/>
+          <Route path='useReducer' element={<UseReducer/>}/>
+          <Route path='useMemo' element={<UseMemo/>}/>
+          <Route path='usecallback' element={<UseCallback/>}/>
         </Route>
       </Routes>
-    </>
+    </>  
   )
 }
-
 export default App
