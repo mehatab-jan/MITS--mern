3447ec1faa1abd1a426653027e662fc0d56d0548
mehatab <mehatab661@gmail.com>
2026-02-06 12:48:08 +0530

    connected with todo

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 import App from './App.jsx'
 import ThemeProvider from './context/ThemeProvider.jsx'
-import AuthProvider from './Pages/About.jsx'
+import AuthProvider from './context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
