cd20821777c6f97b7eada4d75ce15ac97be45acf
mehatab <mehatab661@gmail.com>
2026-02-06 10:51:32 +0530

    backend updated

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,32 +1,11 @@
-// import { BrowserRouter, Route, Routes } from 'react-router-dom'
-// import Navbar from './component/Navbar'
-// import Home from './pages/Home'
-// import Service from './pages/Service'
-// import Contact from './pages/Contact'
-// import About from './pages/About'
-// import Counter from './component/Counter'
-// import Login from './Auth/Login'
-// import UseState from './Hooks/UseState'
+// 
 
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
 
-//       <Routes>
-//         <Route path='/' element={<Home name="Nammu" />} />
-//         <Route path='/about' element={<About name="Nammu" age={20} dept="CS&DS" />} />
-//         <Route path='/contact' element={<Contact />} />
-//         <Route path='/service' element={<Service />} />
-//         <Route path='/counter' element={<Counter />} />
-//         <Route path='/useState' element={<UseState />} />
-//         <Route path='/login' element={<Login />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
 
-// export default App
+
+
+
+
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './component/Navbar'
 import Home from './Pages/Home'
@@ -43,6 +22,7 @@ import UseRef from './Hooks/UseRef'
 import UseReducer from './Hooks/UseReducer'
 import UseMemo from './Hooks/UseMemo'
 import UseCallback from './Hooks/UseCallback'
+import Todo from './component/Todo'   // ✅ FIXED (React component)
 
 const App = () => {
   return (
@@ -53,9 +33,10 @@ const App = () => {
         <Route path="/" element={<Home name="Jaaan" />} />
         <Route path="/about" element={<About name="Jaaan" age={20} dept="CSD" />} />
         <Route path="/contact" element={<Contact />} />
-         <Route path="/services" element={<Service />} />
+        <Route path="/services" element={<Service />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/todo" element={<Todo />} />
 
         {/* Hooks */}
         <Route path="/hook" element={<Hook />}>
@@ -64,10 +45,8 @@ const App = () => {
           <Route path="useEffectApi" element={<UseEffectApi />} />
           <Route path="useRef" element={<UseRef />} />
           <Route path="useReducer" element={<UseReducer />} />
-          
           <Route path="useMemo" element={<UseMemo />} />
           <Route path="useCallback" element={<UseCallback />} />
-
         </Route>
       </Routes>
     </>
@@ -75,5 +54,3 @@ const App = () => {
 }
 
 export default App
-
-
--- React/src/component/Navbar.jsx
+++ React/src/component/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
                 <li><Link className='link' to="/hook">Hook</Link></li>
                 <li><Link className='link' to="/login">Login</Link></li>
                 <li><Link className='link' to="/signup">Signup</Link></li>
+                <li><Link className='link' to="/todo">Todo</Link></li>
     
             </ul>
         </nav>
